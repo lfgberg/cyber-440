8e12a1538b73386c61450f82bb6edef731fed28d
lfgberg <lfgaberg@gmail.com>
2024-09-29 23:58:20 -0400

    sadfjkgb

--- pcap-parsing/README.md
+++ pcap-parsing/README.md
@@ -6,7 +6,4 @@ I'm provided with a PCAP full of only headers, no application data. It's huge.
 
 1. I ran BruteShark on the dataset to get a network diagram and a JSON of open ports
 2. Diagram was too cluttered and the open ports included ephemeral ports
-3. Created a python script to parse the JSON and build a list of open ports excluding ephemeral ports
-   1. Add src/dst host to dict if not present
-   2. if destport is not ephemeral and is not present, add to dict
-   3. json dump dict
+3. Created a python script to parse the JSON and build a list of open ports excluding ephemeral ports, it does some connection tracking also
--- pcap-parsing/pcap-parser.py
+++ pcap-parsing/pcap-parser.py
@@ -1,9 +1,6 @@
 import json
-import time
 
 def main():
-    # Start the clock
-    start = time.time()
 
     # Load the JSON file
     data = readJson('network-map.json')
@@ -12,42 +9,97 @@ def main():
     results = parseData(data)
 
     # Save the results
-    saveResults(results, 'results.json')
-
-    # Stop the clock
-    end = time.time()
-
-    print("The time of execution of above program is :",
-      (end-start) * 10**3, "ms")
+    saveResults(results, 'results.txt')
     
     exit()
 
+class Host(object):
+    def __init__(self, ip):
+        self.ip = ip
+
+        self.tcpSent = []
+        self.tcpRecieved = []
+        self.tcpOpen = []
+
+        self.udpSent = []
+        self.udpRecieved = []
+        self.udpOpen = []
+    
+    def __eq__(self, other):
+        return isinstance(other, Host) and self.ip == other.ip
+
+    # TCP
+
+    # If we haven't seen the port before add to sent, if it's been sent and recieved then it's open
+    def addTcpSendPort(self, port):
+         if port not in self.tcpSent:
+            self.tcpSent.append(port)
+            if (port in self.tcpRecieved) and (port not in self.tcpOpen): 
+                self.tcpOpen.append(port)
+            elif int(port) <= 1023 and (port not in self.tcpOpen):
+                self.tcpOpen.append(port)
+
+    # If we haven't seen the port before add to recieved, if it's well known add to open
+    def addTcpRecievedPort(self, port):
+        if port not in self.tcpRecieved:
+            self.tcpRecieved.append(port)
+
+    # UDP
+
+    # If we haven't seen the port before add to sent, if it's been sent and recieved then it's open
+    def addUdpSendPort(self, port):
+        if port not in self.udpSent:
+            self.udpSent.append(port)
+            if (port in self.udpRecieved) and (port not in self.udpOpen): 
+                self.udpOpen.append(port)
+            elif int(port) <= 1023 and (port not in self.udpOpen):
+                self.udpOpen.append(port)
+
+    # If we haven't seen the port before add to recieved, if it's well known add to open
+    def addUdpRecievedPort(self, port):
+        if port not in self.udpRecieved:
+            self.udpRecieved.append(port)
+
+
 def saveResults(results, fileName):
     with open(fileName, 'w') as outfile:
-        json.dump(results, outfile, indent=4)
+        for ip, host in results.items():
+            # Check if there are any open TCP or UDP ports
+            if host.tcpOpen or host.udpOpen:
+                outfile.write(f"IP: {ip}\n")
+                
+                # Print open TCP ports
+                if host.tcpOpen:
+                    outfile.write(f"  Open TCP Ports: {', '.join(map(str, host.tcpOpen))}\n")
+                
+                # Print open UDP ports
+                if host.udpOpen:
+                    outfile.write(f"  Open UDP Ports: {', '.join(map(str, host.udpOpen))}\n")
+
+                outfile.write("\n")
 
 def parseData(data):
     results = {}
 
     for entry in data:
-        # 1. check to see if we've seen the src host before
-        if entry['Source'] not in results:
-            results[entry['Source']] = {'UDP': [], 'TCP': []}
+        srcIp = entry['Source']
+        destIp = entry['Destination']
+        protocol = entry['Protocol']
+        srcPort = entry['SrcPort']
+        destPort = entry['DestPort']
 
-        # 2. check to see if the port is open on the src host & add if so
-        if ((entry['SrcPort'] < entry['DestPort']) & (entry['SrcPort'] <= 49151)):
+        # Create new hosts in results if needed
+        if srcIp not in results:
+            results[srcIp] = Host(srcIp)
+        if destIp not in results:
+            results[destIp] = Host(destIp)
 
-            # 3. Check TCP or UDP
-            if (entry['Protocol'] == "TCP"):
-                # 4. Check if we've seen before or not
-                if entry['SrcPort'] not in results[entry['Source']]['TCP']:
-                    # 5. Append
-                    results[entry['Source']]['TCP'].append(entry['SrcPort'])
-            else:
-                # 4. Check if we've seen before or not
-                if entry['SrcPort'] not in results[entry['Source']]['UDP']:
-                    # 5. Append
-                    results[entry['Source']]['UDP'].append(entry['SrcPort'])
+        if (protocol == "TCP"):
+            results[srcIp].addTcpSendPort(srcPort)
+            results[destIp].addTcpRecievedPort(destPort)
+        else:
+            results[srcIp].addUdpSendPort(srcPort)
+            results[destIp].addUdpRecievedPort(destPort)
     
     return results
 
